daee9e1ce8f57464d0bd992eb1639d7366318da7
pH0xe <reig.julien@free.fr>
2020-12-04 12:07:20 +0100

    Ajout de 3 trigger pour verifier differente contrainte

--- data/createTrigger.sql
+++ data/createTrigger.sql
@@ -30,7 +30,7 @@ BEGIN
         WHEN ((SELECT nombreInscrit FROM LesEpreuvesView WHERE numEp = OLD.numEp) < 4)
         THEN RAISE(ABORT, 'Moins de 3 inscrit en cas de suppression. Supprimer l''épreuve ou inscrivez d''autre personnes.')
     end;
-end;
+END;
 /
 
 -- Verifier que l'on de l'inscription a une équipe le sportif fait bien partie du bon pays
@@ -38,8 +38,41 @@ CREATE TRIGGER IF NOT EXISTS bon_pays_equipe
     BEFORE INSERT ON LesEquipiers
 BEGIN
     SELECT CASE
-        WHEN ((SELECT pays FROM LesSportifs_base WHERE numSp = NEW.numSp) = (SELECT pays FROM LesEquipes WHERE numEq = 1))
+        WHEN ((SELECT pays FROM LesSportifs_base WHERE numSp = NEW.numSp) <> (SELECT pays FROM LesEquipes WHERE numEq = NEW.numEq))
         THEN RAISE(ABORT, 'Le nouveau membre ne fait pas partie du bon pays.')
     END;
-end;
+END;
+/
+
+-- verifier que le sportif a bien un num entre 1000 et 1500
+CREATE TRIGGER IF NOT EXISTS sportif_num
+    BEFORE INSERT ON LesSportifs_base
+BEGIN
+    SELECT CASE
+        WHEN(NEW.numSp > 1500 OR NEW.numSp < 1000)
+        THEN RAISE(ABORT, 'Numéros du sportif invalide. Doit etre entre 1000 et 1500.')
+    END;
+END;
+/
+
+-- verifier que l'équipe a bien un num entre 1 et 100
+CREATE TRIGGER IF NOT EXISTS equipe_num
+    BEFORE INSERT ON LesEquipiers
+BEGIN
+    SELECT CASE
+        WHEN (NEW.numEq > 100 OR NEW.numEq < 1)
+        THEN RAISE(ABORT, 'Numéros d''équipe invalide. Doit être entre 1 et 100.')
+    END;
+END;
+/
+
+-- au moins 2 sportif dans l'equipe si delete
+CREATE TRIGGER IF NOT EXISTS au_moins_2_equipier
+    BEFORE DELETE ON LesEquipiers
+BEGIN
+    SELECT CASE
+        WHEN ((SELECT nbEquipiersEq FROM LesEquipes WHERE LesEquipes.numEq = OLD.numEq) < 3)
+        THEN RAISE(ABORT, 'Moins de 2 équipier en cas de suppression. Supprimer l''équipe ou inscrivez d''autre personnes.')
+    END;
+END;
 /
